f391eb3aec1545871b85143a60feb7446f9ec6bb
chenyi708 <138554543+chenyi708@users.noreply.github.com>
2023-07-04 13:50:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,7 +28,7 @@ const USER_CONFIG = {
       horoscopeDate: '11-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        //{
+        {
           type: '*生日', name: '小橙子🍊', year: '1999', date: '09-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
